5375018731347e8704f0340ff34986107eaa3453
Constantino Schillebeeckx <constantino@matatu.com>
2016-06-16 07:51:29 -0500

    updated readme

--- demo.php
+++ demo.php
@@ -6,7 +6,6 @@
 
         <!-- CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link href="css/open_sans.css" rel="stylesheet">
         <link href="css/phylogram_d3.css" rel="stylesheet">
 
